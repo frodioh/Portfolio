d1ca454852bd1187ee02d89dfdee28e6859a192f
Rodion Fedotov <fedotov.dev@gmail.com>
2016-07-02 13:54:27 +0500

    slider

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   
-  $(function() {
+  $(function() { //прелоадер---------------------------------------
     var imgs = [],
         percents = 1;
 
@@ -10,7 +10,13 @@ $(document).ready(function() {
           background = $this.css('background-image');
 
       if (background != 'none') {
-        var path = background.replace('url("','').replace('")','');
+        var path = background.replace('url(','').replace(')','');
+        if (path[0] === '"' || path[0] === '\'') {
+          path = path.substr(1);
+        }
+        if (path[path.length-1] === '"' || path[0] === '\'') {
+          path = path.substring(0,path.length-1);
+        }
         if (path) {
           imgs.push(path);
         }
@@ -50,7 +56,7 @@ $(document).ready(function() {
 
   });
 
-  $('.anchor-link').on('click', function(e) {
+  $('.anchor-link').on('click', function(e) { //якорный переход---------
     e.preventDefault();
     var id = $(this).attr('href'),
         top = $(id).offset().top;
@@ -62,43 +68,187 @@ $(document).ready(function() {
 });
 
 window.onload = function() {
-  var card, form, authBtn, hamburger, menu, clickArea;
-  if (document.getElementsByClassName("index-container")[0]) {
-    card = document.getElementsByClassName("index-container")[0];
-  }
-  if (document.getElementsByClassName("login-form")[0]) {
-    form = document.getElementsByClassName("login-form")[0];
-  }
-  if (document.getElementsByClassName("authorization-btn")[0]) {
-    authBtn = document.getElementsByClassName("authorization-btn")[0];
-  }
-  if (document.getElementsByClassName("click-area")[0]) {
-    clickArea = document.getElementsByClassName("click-area")[0];
-  }
-  if (document.getElementsByClassName("hamburger")[0]) {
-    hamburger = document.getElementsByClassName("hamburger")[0];
-  }
-  if (document.getElementsByClassName("menu")[0]) {
-    menu = document.getElementsByClassName("menu")[0];
-  }
 
-  if (authBtn) {
-    clickArea.onclick = function() {
-      card.classList.toggle("index-container--flip");
-      authBtn.classList.toggle("authorization-btn--clicked");
-      clickArea.classList.toggle("click-area--display");
-    };
-    authBtn.onclick = function() {
-      card.classList.toggle("index-container--flip");
-      authBtn.classList.toggle("authorization-btn--clicked");
-      clickArea.classList.toggle("click-area--display");
+  (function() {
+    var card, form, authBtn, hamburger, menu, clickArea;
+    if (document.getElementsByClassName("index-container")[0]) {
+      card = document.getElementsByClassName("index-container")[0];
     }
-  }
-  if (hamburger) {
-    hamburger.onclick = function(e) {
-      e.preventDefault();
-      menu.classList.toggle("menu--active");
-      hamburger.classList.toggle("hamburger--transformed")
+    if (document.getElementsByClassName("login-form")[0]) {
+      form = document.getElementsByClassName("login-form")[0];
     }
-  }
+    if (document.getElementsByClassName("authorization-btn")[0]) {
+      authBtn = document.getElementsByClassName("authorization-btn")[0];
+    }
+    if (document.getElementsByClassName("click-area")[0]) {
+      clickArea = document.getElementsByClassName("click-area")[0];
+    }
+    if (document.getElementsByClassName("hamburger")[0]) {
+      hamburger = document.getElementsByClassName("hamburger")[0];
+    }
+    if (document.getElementsByClassName("menu")[0]) {
+      menu = document.getElementsByClassName("menu")[0];
+    }
+
+    if (authBtn) {
+      clickArea.onclick = function() {
+        card.classList.toggle("index-container--flip");
+        authBtn.classList.toggle("authorization-btn--clicked");
+        clickArea.classList.toggle("click-area--display");
+      };
+      authBtn.onclick = function() {
+        card.classList.toggle("index-container--flip");
+        authBtn.classList.toggle("authorization-btn--clicked");
+        clickArea.classList.toggle("click-area--display");
+      }
+    }
+    if (hamburger) {
+      hamburger.onclick = function(e) {
+        var list;
+        if (document.getElementsByClassName("menu__list")[0]) {
+          list = document.getElementsByClassName("menu__list")[0];
+        }
+        e.preventDefault();
+        menu.classList.toggle("menu--active");
+        if (hamburger.classList.contains('hamburger--transformed')) {
+          list.classList.toggle('menu__list--active');
+        } else {
+          setTimeout(function() {
+            list.classList.toggle('menu__list--active');
+          }, 500);
+        }
+        hamburger.classList.toggle("hamburger--transformed")
+      }
+    }
+  }());
+
+  (function() {
+  //------------------------
+    var downBtn = $('.thumbnail.thumbnail--down'),
+        upBtn = $('.thumbnail.thumbnail--up'),
+        slide = $('.slider__current'),
+        counterDown = 0,
+        counterUp = 2,
+        counterSlide = 1;
+    
+    downBtn.on('click', function() { //Слайдер вверх ---------------
+      counterDown--;
+      counterUp--;
+      counterSlide--;
+      var $this = $(this),
+          container = $this.closest('.slider'),
+          downWindow = $this.find('.thumbnail__list'),
+          itemsDown = downBtn.find('.thumbnail__item'),
+          itemsUp = upBtn.find('.thumbnail__item'),
+          itemsSlide = slide.find('.slider__current-item'),
+          activeItemDown = downBtn.find('.thumbnail__item--active'),
+          activeItemUp = upBtn.find('.thumbnail__item--active'),
+          activeItemSlide = slide.find('.slider__current-item--active');
+
+      if (counterDown < 0) {
+        counterDown = itemsDown.length-1;
+      }
+
+      if (counterUp < 0) {
+        counterUp = itemsUp.length-1;
+      }
+
+      if (counterSlide < 0) {
+        counterSlide = itemsUp.length-1;
+      }
+
+      var reqItemDown = itemsDown.eq(counterDown),
+          reqItemUp = itemsUp.eq(counterUp),
+          reqItemSlide = itemsSlide.eq(counterSlide);
+
+      activeItemDown.animate({
+        'top': '100%'
+      }, 300);
+
+      activeItemUp.animate({
+        'top' : '-100%'
+      }, 300);
+
+      activeItemSlide.fadeOut(700);
+      reqItemSlide.fadeIn(700);
+      activeItemSlide.removeClass('slider__current-item--active').css('opacity', '0');
+      reqItemSlide.addClass('slider__current-item--active');
+
+      reqItemDown.animate({
+        'top' : '0'
+      }, 300, function() {
+        activeItemDown.removeClass('thumbnail__item--active').css('top', '-100%');
+        reqItemDown.addClass('thumbnail__item--active');
+      });
+
+      reqItemUp.animate({
+        'top' : '0'
+      }, 300, function() {
+        activeItemUp.removeClass('thumbnail__item--active').css('top', '100%');
+        reqItemUp.addClass('thumbnail__item--active');
+      });
+    });
+
+    upBtn.on('click', function() { //Слайдер вниз ---------------------
+      counterDown++;
+      counterUp++;
+      counterSlide++;
+      var $this = $(this),
+          container = $this.closest('.slider'),
+          upWindow = $this.find('.thumbnail__list'),
+          itemsDown = downBtn.find('.thumbnail__item'),
+          itemsUp = upBtn.find('.thumbnail__item'),
+          itemsSlide = slide.find('.slider__current-item'),
+          activeItemDown = downBtn.find('.thumbnail__item--active'),
+          activeItemUp = upBtn.find('.thumbnail__item--active'),
+          activeItemSlide = slide.find('.slider__current-item--active');
+
+      if (counterUp >= itemsUp.length) {
+        counterUp = 0;
+      }
+
+      if (counterDown >= itemsDown.length) {
+        counterDown = 0;
+      }
+
+      if (counterSlide >= itemsDown.length) {
+        counterSlide = 0;
+      }
+
+      var reqItemDown = itemsDown.eq(counterDown),
+          reqItemUp = itemsUp.eq(counterUp),
+          reqItemSlide = itemsSlide.eq(counterSlide);
+
+      activeItemDown.animate({
+        'top': '100%'
+      }, 300);
+
+      activeItemUp.animate({
+        'top' : '-100%'
+      }, 300);
+
+      activeItemSlide.fadeOut(700);
+      reqItemSlide.fadeIn(700);
+      activeItemSlide.removeClass('slider__current-item--active').css('opacity', '0');
+      reqItemSlide.addClass('slider__current-item--active');
+
+      reqItemDown.animate({
+        'top' : '0'
+      }, 300, function() {
+        activeItemDown.removeClass('thumbnail__item--active').css('top', '-100%');
+        reqItemDown.addClass('thumbnail__item--active');
+      });
+      reqItemUp.animate({
+        'top' : '0'
+      }, 300, function() {
+        activeItemUp.removeClass('thumbnail__item--active').css('top', '100%');
+        reqItemUp.addClass('thumbnail__item--active');
+      });
+    });
+
+
+  //------------------------
+
+  }());
+
 }
